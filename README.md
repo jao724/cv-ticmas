5c6d5eb87374862122ae924ef083934bb4c4fc6c
jao <joseantonioorellana@gmail.com>
2023-01-22 16:07:53 -0300

    primer commit

---
+++ README.md
@@ -0,0 +1,1 @@
+primer pagina web
---
+++ portfolio.js
@@ -0,0 +1,11 @@
+
+
+document.getElementById('boton_color').addEventListener('click', function () {
+    document.body.style.backgroundColor = '#4dff4d';
+
+});
+
+
+
+
+
